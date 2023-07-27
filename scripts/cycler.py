daebd17d7d63a8eb37409aaece927f263481b315
Niklas <nrol@kth.se>
2023-07-27 11:12:26 +0200

    Added configurations to cycler

--- scripts/cycler.py
+++ scripts/cycler.py
@@ -10,20 +10,97 @@ from Norbit_FLS_driver.cfg import fls_paramsConfig
 from rospy_message_converter import message_converter
 from Norbit_FLS_driver.msg import Configs
 
+from copy import deepcopy
+
 def create_configs():
     configlist = []
     config1 = fls_paramsConfig.defaults.copy()
+    
+    #Standard stuff
     config1['set_power_mode'] = 1
-    config2 = fls_paramsConfig.defaults.copy()
-    config2['set_vga'] = 0
-    config2['set_gain'] = 30
-    config2['set_power_mode'] = 1
-    config3 = fls_paramsConfig.defaults.copy()
-    config3['set_tx_Frequency'] = 200
-    config3['set_power_mode'] = 1
+    config1['set_range_min'] = 0
+    config1['set_range_max'] = 200
+    config1['set_opening_angle'] = 150
+    config1['set_vertical_resolution'] = 2048
+    config1['set_horizontal_resolution'] = 512
+    config1['set_tx_amp'] = 15
+    config1['set_rate'] = 5
+    config1['set_tx_Frequency'] = 400
+
+    #stuff to change
+    config1['set_mf_decimation'] = 1
+    config1['set_tx_Bandwidth'] = 80
+    config1['set_tx_pulse_length'] = 500
+    config1['set_gain'] = 0
+    config1['set_vga'] = 50
+
+    #decimation test
+    config02 = deepcopy(config1)
+    config02['set_mf_decimation'] = 8
+    config03 = deepcopy(config1)
+    config03['set_mf_decimation'] = 16
+
+    #tx bandwidth
+    config04 = deepcopy(config1)
+    config04['set_tx_Bandwidth'] = 40
+    config05 = deepcopy(config1)
+    config05['set_tx_Bandwidth'] = 20
+    config06 = deepcopy(config1)
+    config06['set_tx_Bandwidth'] = 0
+
+    #pulse length 10-819
+    config07 = deepcopy(config1)
+    config07['set_tx_pulse_length'] = 10
+    config08 = deepcopy(config1)
+    config08['set_tx_pulse_length'] = 100
+    config09 = deepcopy(config1)
+    config09['set_tx_pulse_length'] = 250
+    config10 = deepcopy(config1)
+    config10['set_tx_pulse_length'] = 800
+
+    #gain
+    config11 = deepcopy(config1)
+    config11['set_gain'] = 0
+    config11['set_vga'] = 0
+    config12 = deepcopy(config1)
+    config12['set_gain'] = 33
+    config12['set_vga'] = 0
+    config13 = deepcopy(config1)
+    config13['set_gain'] = 66
+    config13['set_vga'] = 0
+    config14 = deepcopy(config1)
+    config14['set_gain'] = 100
+    config14['set_vga'] = 0
+
+    #VGA
+    config15 = deepcopy(config1)
+    config15['set_vga'] = 25
+    config16 = deepcopy(config1)
+    config16['set_vga'] = 50
+    config17 = deepcopy(config1)
+    config17['set_vga'] = 75
+    config18 = deepcopy(config1)
+    config18['set_vga'] = 100
+    
     configlist.append(config1)
-    configlist.append(config2)
-    configlist.append(config3) # last config in the list should be the default config if you want it in the loop
+    configlist.append(config02)
+    configlist.append(config03)
+    configlist.append(config04)
+    configlist.append(config05)
+    configlist.append(config06)
+    configlist.append(config07)
+    configlist.append(config08)
+    configlist.append(config09)
+    configlist.append(config10)
+    configlist.append(config11)
+    configlist.append(config12)
+    configlist.append(config13)
+    configlist.append(config14)
+    configlist.append(config15)
+    configlist.append(config16)
+    configlist.append(config17)
+    configlist.append(config18)
+
     return configlist
 
 
